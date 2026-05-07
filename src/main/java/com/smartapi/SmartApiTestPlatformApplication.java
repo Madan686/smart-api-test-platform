26fcb23d224746985a7e33f3e665a0bc2edc42f4
Madan686 <madan.manju634@gmail.com>
2026-05-07 20:24:47 +0530

    first commit

---
+++ src/main/java/com/smartapi/SmartApiTestPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.smartapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartApiTestPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartApiTestPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smartapi/SmartApiTestPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartApiTestPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
